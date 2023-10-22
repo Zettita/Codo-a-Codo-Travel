98ec4e661e077a9a0228feb554cfe92979ceeb88
Cristian I <ciriel@gillian5.local>
2023-10-21 22:16:57 -0300

    Agregado el href a contacto.html, form en progreso

---
+++ JS/contacto.js
@@ -0,0 +1,17 @@
+function validar(){
+
+    let esValido = true
+    // flag, si alguno de los campos en invalido se cambia a false
+    // al final de la función si aún es True, entonces llama al Submit
+    // del formulario, de lo contrario marca los campos invalidos
+
+    let formulario = document.formContacto
+    let nombre = document.forms["formContacto"]["nombre"].value
+    if (nombre == "")
+    
+
+
+
+
+
+}
